44be716787d15b29164f5c2e8d115d92a40427c9
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-05-02 02:54:36 +0530

    change

--- src/services/mongoDB/connectDB.js
+++ src/services/mongoDB/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URL);
-    console.log("Connected to db");
+    console.log("Connected to the  Database");
   } catch (error) {
     console.log(error);
   }
