48614bae01c0eb5485eb724942de0503039474de
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-05-05 21:50:25 +0530

    change

--- src/services/mongoDB/connectDB.js
+++ src/services/mongoDB/connectDB.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to the  Database");
   } catch (error) {
     console.log(error);
